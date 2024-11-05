8e9b5ea6e74cb5e3bde58497c33097e9349f3966
Le Ha Thanh Son <86293490+sonbongda@users.noreply.github.com>
2024-11-05 22:04:53 +0700

    Update prepare_lists.py

--- data/prepare_lists.py
+++ data/prepare_lists.py
@@ -2,6 +2,12 @@ import argparse
 import os
 import pandas as pd
 
+import re
+
+
+
+pattern = r"^(.*?)/"
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Options')
     parser.add_argument('--data_path',
@@ -11,20 +17,19 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     df = pd.read_csv('./train_list.txt')
-    df['rgb_path'] = df.rgb_path.apply(lambda x: os.path.join(args.data_path, x))
-    df['ir_path'] = df.ir_path.apply(lambda x: os.path.join(args.data_path, x))
-    df['depth_path'] = df.depth_path.apply(lambda x: os.path.join(args.data_path, x))
+    df['rgb_path'] = df.rgb_path.apply(lambda x: os.path.join(args.data_path,re.match(pattern, x).group(1), x))
+    df['ir_path'] = df.ir_path.apply(lambda x: os.path.join(args.data_path,re.match(pattern, x).group(1), x))
+    df['depth_path'] = df.depth_path.apply(lambda x: os.path.join(args.data_path,re.match(pattern, x).group(1), x))
     df.to_csv('./train_list.txt', index=False)
 
     df = pd.read_csv('./dev_list.txt')
-    df['rgb_path'] = df.rgb_path.apply(lambda x: os.path.join(args.data_path, x))
-    df['ir_path'] = df.ir_path.apply(lambda x: os.path.join(args.data_path, x))
-    df['depth_path'] = df.depth_path.apply(lambda x: os.path.join(args.data_path, x))
+    df['rgb_path'] = df.rgb_path.apply(lambda x: os.path.join(args.data_path,re.match(pattern, x).group(1), x))
+    df['ir_path'] = df.ir_path.apply(lambda x: os.path.join(args.data_path,re.match(pattern, x).group(1), x))
+    df['depth_path'] = df.depth_path.apply(lambda x: os.path.join(args.data_path,re.match(pattern, x).group(1), x))
     df.to_csv('./dev_list.txt', index=False)
 
-    df = pd.read_csv('./dev_test_list.txt')
-    df['rgb_path'] = df.rgb_path.apply(lambda x: os.path.join(args.data_path, x))
-    df['ir_path'] = df.ir_path.apply(lambda x: os.path.join(args.data_path, x))
-    df['depth_path'] = df.depth_path.apply(lambda x: os.path.join(args.data_path, x))
-    df.to_csv('./dev_test_list.txt', index=False)
-
+    df = pd.read_csv('./dev_test_list_v1.txt')
+    df['rgb_path'] = df.rgb_path.apply(lambda x: os.path.join(args.data_path,re.match(pattern, x).group(1), x))
+    df['ir_path'] = df.ir_path.apply(lambda x: os.path.join(args.data_path,re.match(pattern, x).group(1), x))
+    df['depth_path'] = df.depth_path.apply(lambda x: os.path.join(args.data_path,re.match(pattern, x).group(1), x))
+    df.to_csv('./dev_test_list_v1.txt', index=False)
